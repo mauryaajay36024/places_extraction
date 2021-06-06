52fd6fed9823a33d19dd87ec16f69dc52b04dbda
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-06 10:43:08 +0530

    PlacesExtractionImp modified

--- src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionImp.java
+++ src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionImp.java
@@ -1,16 +1,19 @@
 package com.near.platform.placesExtraction.service;
 
+import com.near.platform.placesExtraction.exception.*;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.repository.PlacesExtractionRepository;
+import mcm.MessageCodeCategory;
+import mcm.MessageCodeInfo;
+import mcm.NearServiceResponseDto;
 import mcm.NearServiceResponseUtil;
-import org.apache.commons.lang.NullArgumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.MissingResourceException;
-import java.util.Optional;
 
 @Service
 public class PlacesExtractionImp {
@@ -24,76 +27,83 @@ public class PlacesExtractionImp {
   @Autowired
   PlacesExtractionRepository placesExtractionRepository;
 
-  public String addDataToDatabase(LocationMetrics locationMetrics) {
-
-    logger.info("Inside addDataToDatabase method");
-
+  public ResponseEntity<NearServiceResponseDto> addDataToDatabase(LocationMetrics locationMetrics) throws Exception {
+    MessageCodeInfo messageCodeInfo;
+    NearServiceResponseDto nearServiceResponseDto;
     //To check data is valid
-    if(dataValidator(locationMetrics)){
-      try {
-        logger.info("Data is valid");
-        placesExtractionRepository.save(locationMetrics);
-        logger.info("Inside addDataToDatabase method ::Data saved to database");
-        return "data added to database successfully";
-      }
-      catch (Exception ex){
-        logger.error("Exception while saving data to database",ex);
-      }
+    dataValidator(locationMetrics);
+    try {
+      placesExtractionRepository.save(locationMetrics);
+      //todo add response entity
+      messageCodeInfo = nearServiceResponseUtil.fetchMessageCodeInfo(MessageCodeCategory.PLATFORM, "PLT-0087", null);
+      nearServiceResponseDto = nearServiceResponseUtil.buildNearServiceResponseDto(true, HttpStatus.OK.value(), "PLT-0087",
+          messageCodeInfo.getLongDesc(),
+          messageCodeInfo.getShortDesc(),
+          messageCodeInfo.getCodeType(), "Data added to database successfully");
+      return new ResponseEntity<>(nearServiceResponseDto, HttpStatus.OK);
+    }
+    catch (Exception ex){
+      logger.error("Exception while saving data to database",ex);
     }
    //todo response entity
-    return "data saving failed";
+    messageCodeInfo = nearServiceResponseUtil.fetchMessageCodeInfo(MessageCodeCategory.PLATFORM, "PLT-0001", null);
+    nearServiceResponseDto = nearServiceResponseUtil.buildNearServiceResponseDto(true, HttpStatus.PRECONDITION_FAILED.value(), "PLT-0001",
+        messageCodeInfo.getLongDesc(),
+        messageCodeInfo.getShortDesc(),
+        messageCodeInfo.getCodeType(), "Uploaded file is empty or invalid");
+    return new ResponseEntity<>(nearServiceResponseDto, HttpStatus.PRECONDITION_FAILED);
+
   }
 
-  public String updateDataToDatabase(String id, LocationMetrics locationMetricsDetails) {
-      if (dataValidator(locationMetricsDetails)) {
-        logger.info("Data is valid");
-        Optional<LocationMetrics> locationMetrics1 = placesExtractionRepository.findById(id);
-        if (locationMetrics1.isPresent()) {
-          try {
-            LocationMetrics locationMetrics = locationMetrics1.get();
+  public String updateData(String id, LocationMetrics locationMetricsDetails) throws Exception {
+      dataValidator(locationMetricsDetails);
 
-            locationMetrics.setCurrentPOICount(locationMetricsDetails.getCurrentPOICount());//set current poi count
-            locationMetrics.setExtractedPOICount(locationMetricsDetails.getExtractedPOICount());//set extracted poi count
-            locationMetrics.setIngestCount(locationMetricsDetails.getIngestCount()); //set ingest count
-            locationMetrics.setUpdateCount(locationMetricsDetails.getUpdateCount());// set updated count
-            locationMetrics.setDeleteCount(locationMetricsDetails.getDeleteCount()); //set delete count
-            locationMetrics.setDateOfExtraction(locationMetricsDetails.getDateOfExtraction()); //set date of extraction
-            locationMetrics.setPoiListId(locationMetricsDetails.getPoiListId()); //set poi list id
-            locationMetrics.setCountry(locationMetricsDetails.getCountry());//set country
-            locationMetrics.setGroundTruth(locationMetricsDetails.getGroundTruth());//set ground truth
-            locationMetrics.setSource(locationMetricsDetails.getSource());//set source
+      if (placesExtractionRepository.findById(id).isPresent()) {
+        try {
+          LocationMetrics locationMetrics = placesExtractionRepository.findById(id).get();
 
-            placesExtractionRepository.save(locationMetrics);
-            logger.info("Data updated into database successfully");
-            return "Data updated successfully";
-          } catch (Exception ex) {
-            logger.error("Exception while updating data :", ex);
-          }
+          locationMetrics.setCurrentPOICount(locationMetricsDetails.getCurrentPOICount());//set current poi count
+          locationMetrics.setExtractedPOICount(locationMetricsDetails.getExtractedPOICount());//set extracted poi count
+          locationMetrics.setIngestCount(locationMetricsDetails.getIngestCount()); //set ingest count
+          locationMetrics.setUpdateCount(locationMetricsDetails.getUpdateCount());// set updated count
+          locationMetrics.setDeleteCount(locationMetricsDetails.getDeleteCount()); //set delete count
+          locationMetrics.setDateOfExtraction(locationMetricsDetails.getDateOfExtraction()); //set date of extraction
+          locationMetrics.setPoiListId(locationMetricsDetails.getPoiListId()); //set poi list id
+          locationMetrics.setCountry(locationMetricsDetails.getCountry());//set country
+          locationMetrics.setGroundTruth(locationMetricsDetails.getGroundTruth());//set ground truth
+          locationMetrics.setSource(locationMetricsDetails.getSource());//set source
+
+          placesExtractionRepository.save(locationMetrics);
+          //todo return response entity instead string
+          return "Data updated successfully";
+        } catch (Exception ex) {
+          logger.error("Exception while updating data :", ex);
         }
       }
-      return "Data not updated";
+        throw new IdNotFoundException("Entered id doesn't matched, please provide correct id");
   }
 
 
-  public LocationMetrics getLocationMetricsData(String id) {
-    Optional<LocationMetrics> locationMetrics1 = placesExtractionRepository.findById(id);
-    if(locationMetrics1.isPresent()){
-      LocationMetrics locationMetrics=locationMetrics1.get();
-      return locationMetrics;
+  public LocationMetrics getLocationMetricsData(String id) throws IdNotFoundException {
+
+    if(placesExtractionRepository.findById(id).isPresent()){
+      return placesExtractionRepository.findById(id).get();
     }
-    throw new MissingResourceException("No data available for ::",id,"into database");
+    throw new IdNotFoundException("Entered id doesn't  matched, please provide correct id");
   }
 
-  private boolean dataValidator(LocationMetrics locationMetrics) {
-    if(locationMetrics !=null) {
-      String country = locationMetrics.getCountry();
-      Long extractedPoiCount = locationMetrics.getExtractedPOICount();
-      Long deletedCount = locationMetrics.getDeleteCount();
-      if (country != null && extractedPoiCount != null && deletedCount != null) {
-        return true;
-      }
-      throw new IllegalArgumentException("Critical values are missing, Provide all required values");
+  private void dataValidator(LocationMetrics locationMetrics)  throws Exception{
+    if (locationMetrics.getCountry() == null) {
+      throw new CountryNotFoundException("Country value is missing");
     }
-    throw new NullArgumentException("Location metrics is null, Please provide value");
+    if(locationMetrics.getExtractedPOICount() == null){
+      throw new ExtractedPoiCountNotFoundException("Extracted Poi Count is missing");
+    }
+    if(locationMetrics.getDeleteCount() == null){
+      throw new DeletedCountNotFoundException("DeletedCount is missing");
+    }
+    if(locationMetrics.getCurrentPOICount() == null){
+      throw new CurrentPoiCountNotFoundException("Current poi count is missing");
+    }
   }
 }
