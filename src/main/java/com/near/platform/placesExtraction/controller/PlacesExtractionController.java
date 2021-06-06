68cecf3fd47311a817099e9d4293b2605eb5d1a6
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-06 10:43:36 +0530

    PlacesExtractionController modified

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -1,12 +1,16 @@
 package com.near.platform.placesExtraction.controller;
 
+import com.near.platform.placesExtraction.exception.CountryNotFoundException;
+import com.near.platform.placesExtraction.exception.IdNotFoundException;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.service.PlacesExtractionImp;
 import io.swagger.annotations.ApiOperation;
+import mcm.NearServiceResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,34 +19,21 @@ public class PlacesExtractionController {
 
     Logger logger = LoggerFactory.getLogger(PlacesExtractionController.class);
 
-
     @Autowired
     PlacesExtractionImp placesExtractionImp;
 
-    //Adding location metrics data to database
     @ApiOperation(value = "Add metrics fields into Mongodb database")
-    @PostMapping(value = "/addMetricsData")
-    public String addMetricsDataToMongoDatabase(@RequestBody LocationMetrics locationMetrics) {
+    @PostMapping(value = "/metrics")
+    public ResponseEntity<NearServiceResponseDto> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
 
-        logger.info("Request received for add metrics data to mongodb database");
-        placesExtractionImp.addDataToDatabase(locationMetrics);
-
-        String response = placesExtractionImp.addDataToDatabase(locationMetrics);
-        logger.info("Data added to database successfully");
         //todo change response entity
-        return response;
-
+        return placesExtractionImp.addDataToDatabase(locationMetrics);
     }
 
-    //Update location metrics
     @ApiOperation(value = "Update metrics field data ")
-    @PutMapping(value ="/updateMetrics/{id}")
-
-    public String updateMetricsDataToMongoDatabase(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) {
-        logger.info("Request received for update metrics Data ");
-
-        String response=placesExtractionImp.updateDataToDatabase(id, locationMetrics);
-        logger.info("Data updated to database successfully");
+    @PutMapping(value ="/metrics/{id}")
+    public String updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
+        String response=placesExtractionImp.updateData(id, locationMetrics);
 
         //todo change response
         return response;
@@ -51,13 +42,10 @@ public class PlacesExtractionController {
 
     //Fetch location metrics data
     @ApiOperation(value = "Fetch metrics data for given id")
-    @GetMapping(value = "/getMetrics/{id}")
-
-    public LocationMetrics getMetricsData(@PathVariable String id)  {
-        logger.info("Request received to fetch the metrics Data ");
+    @GetMapping(value = "/metrics/{id}")
+    public LocationMetrics getMetricsData(@PathVariable String id) throws IdNotFoundException {
         LocationMetrics response=placesExtractionImp.getLocationMetricsData(id);
 
-        logger.info("Data fetched from database successfully");
         return response;
     }
 }
