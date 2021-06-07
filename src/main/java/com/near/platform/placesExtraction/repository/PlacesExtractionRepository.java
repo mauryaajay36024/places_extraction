200327b667a668cfcb66c1d083a1c8c5a8aabe2d
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-07 14:13:56 +0530

    custom method added

--- src/main/java/com/near/platform/placesExtraction/repository/PlacesExtractionRepository.java
+++ src/main/java/com/near/platform/placesExtraction/repository/PlacesExtractionRepository.java
@@ -3,9 +3,12 @@ package com.near.platform.placesExtraction.repository;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 
 @Repository
 public interface PlacesExtractionRepository extends MongoRepository<LocationMetrics, String> {
 
+  List<LocationMetrics> findByPoiListId(Long poiListId);
 }
