fb1584b869addb1498c4acf9737e1887ae40a943
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-07 08:24:25 +0530

    file deleted

--- src/main/java/com/near/platform/placesExtraction/dto/ExampleDto.java
+++
@@ -1,9 +0,0 @@
-package com.near.platform.placesExtraction.dto;
-
-import java.io.Serializable;
-
-/**
- * Created by Prathap on 18 Jul, 2019
- */
-public class ExampleDto implements Serializable {
-}
