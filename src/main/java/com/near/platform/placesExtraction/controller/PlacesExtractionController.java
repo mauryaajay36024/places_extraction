a4e12e23dff3d7de320b68c457f8a869e8305d26
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-07 12:38:54 +0530

    response changed to nearServiceResponseDto

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -3,10 +3,10 @@ package com.near.platform.placesExtraction.controller;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.service.PlacesExtractionImp;
 import io.swagger.annotations.ApiOperation;
+import mcm.NearServiceResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +21,14 @@ public class PlacesExtractionController {
 
     @ApiOperation(value = "Add metrics fields into Mongodb database")
     @PostMapping(value = "/metrics")
-    public ResponseEntity<HttpStatus> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
+    public ResponseEntity<NearServiceResponseDto> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
         return placesExtractionImp.addDataToDatabase(locationMetrics);
     }
 
     @ApiOperation(value = "Update metrics field data ")
     @PutMapping(value ="/metrics/{id}")
-    public ResponseEntity<HttpStatus> updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
-
+    public ResponseEntity<NearServiceResponseDto> updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
         return placesExtractionImp.updateMetricsDetail(id, locationMetrics);
-
     }
 
     //Fetch location metrics data
