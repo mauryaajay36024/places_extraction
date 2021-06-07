48232eb2a50dc614ea1032443d67a26c540ef4a1
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-07 08:26:27 +0530

    controller updated

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -1,15 +1,12 @@
 package com.near.platform.placesExtraction.controller;
 
-import com.near.platform.placesExtraction.exception.CountryNotFoundException;
-import com.near.platform.placesExtraction.exception.IdNotFoundException;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.service.PlacesExtractionImp;
 import io.swagger.annotations.ApiOperation;
-import mcm.NearServiceResponseDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,28 +21,23 @@ public class PlacesExtractionController {
 
     @ApiOperation(value = "Add metrics fields into Mongodb database")
     @PostMapping(value = "/metrics")
-    public ResponseEntity<NearServiceResponseDto> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
-
-        //todo change response entity
+    public ResponseEntity<HttpStatus> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
         return placesExtractionImp.addDataToDatabase(locationMetrics);
     }
 
     @ApiOperation(value = "Update metrics field data ")
     @PutMapping(value ="/metrics/{id}")
-    public String updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
-        String response=placesExtractionImp.updateData(id, locationMetrics);
+    public ResponseEntity<HttpStatus> updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
 
-        //todo change response
-        return response;
+        return placesExtractionImp.updateMetricsDetail(id, locationMetrics);
 
     }
 
     //Fetch location metrics data
     @ApiOperation(value = "Fetch metrics data for given id")
     @GetMapping(value = "/metrics/{id}")
-    public LocationMetrics getMetricsData(@PathVariable String id) throws IdNotFoundException {
-        LocationMetrics response=placesExtractionImp.getLocationMetricsData(id);
-
-        return response;
+    public LocationMetrics getMetricsData(@PathVariable String id) throws Exception {
+        return placesExtractionImp.getLocationMetricsData(id);
     }
+
 }
