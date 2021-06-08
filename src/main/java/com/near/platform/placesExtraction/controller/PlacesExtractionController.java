1f73eba4b03f643a23255cf1aeb1edf9b3384f8c
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-08 16:53:38 +0530

    Controller class modified

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -1,7 +1,7 @@
 package com.near.platform.placesExtraction.controller;
 
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
-import com.near.platform.placesExtraction.service.PlacesExtractionImp;
+import com.near.platform.placesExtraction.service.PlacesExtractionService;
 import io.swagger.annotations.ApiOperation;
 import mcm.NearServiceResponseDto;
 import org.slf4j.Logger;
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin
 @RestController
 @RequestMapping("/v1/places")
 public class PlacesExtractionController {
@@ -17,25 +20,32 @@ public class PlacesExtractionController {
     Logger logger = LoggerFactory.getLogger(PlacesExtractionController.class);
 
     @Autowired
-    PlacesExtractionImp placesExtractionImp;
+    PlacesExtractionService placesExtractionService;
 
     @ApiOperation(value = "Add metrics fields into Mongodb database")
     @PostMapping(value = "/metrics")
     public ResponseEntity<NearServiceResponseDto> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
-        return placesExtractionImp.addDataToDatabase(locationMetrics);
+        return placesExtractionService.addMetricsDataToDatabase(locationMetrics);
     }
 
     @ApiOperation(value = "Update metrics field data ")
-    @PutMapping(value ="/metrics/{id}")
-    public ResponseEntity<NearServiceResponseDto> updateMetricsData(@PathVariable String id,@RequestBody LocationMetrics locationMetrics) throws Exception {
-        return placesExtractionImp.updateMetricsDetail(id, locationMetrics);
+    @PutMapping(value ="/metrics/")
+    public ResponseEntity<NearServiceResponseDto> updateMetricsData( @RequestParam(value = "id") Long id,@RequestBody LocationMetrics locationMetrics) throws Exception {
+        return placesExtractionService.updateMetricsDetail(id, locationMetrics);
     }
 
     //Fetch location metrics data
+
     @ApiOperation(value = "Fetch metrics data for given id")
-    @GetMapping(value = "/metrics/{id}")
-    public LocationMetrics getMetricsData(@PathVariable String id) throws Exception {
-        return placesExtractionImp.getLocationMetricsData(id);
+    @GetMapping(value = "/metrics/")
+    public LocationMetrics getMetricsData(@RequestParam(value = "id") Long id) throws Exception {
+        return placesExtractionService.getLocationMetricsData(id);
     }
 
+    //Fetch location metrics data
+    @ApiOperation(value = "Fetch all metrics data")
+    @GetMapping(value = "/metrics/all")
+    public List<LocationMetrics> getAllMetricsData() throws Exception{
+        return placesExtractionService.getAllLocationMetricsData();
+    }
 }
