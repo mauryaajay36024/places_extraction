78d6670bf2283cbfc463750f289c22090f053b54
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-08 17:01:21 +0530

    MetricsDataNotFoundException added

---
+++ src/main/java/com/near/platform/placesExtraction/exception/MetricsDataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.near.platform.placesExtraction.exception;
+
+public class MetricsDataNotFoundException extends Exception{
+  public MetricsDataNotFoundException(String message){
+    super(message);
+  }
+}
