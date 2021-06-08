1581447caaf28672ba11e26ec4d0b20dda7c8617
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-08 16:58:38 +0530

    Consul config added

---
+++ src/main/java/com/near/platform/placesExtraction/config/ConsulConfig.java
@@ -0,0 +1,23 @@
+package com.near.platform.placesExtraction.config;
+
+import com.ecwid.consul.v1.ConsulClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import utility.service.NearConsulService;
+
+
+@Configuration
+public class ConsulConfig {
+
+  @Autowired
+  private NearConsulService nearConsulService;
+  @Autowired
+  private ApplicationConfig applicationConfig;
+
+  @Bean
+  public ConsulClient getConsulClient() {
+    return nearConsulService.getConsulClient(applicationConfig.getConsul().getHost());
+  }
+
+}
