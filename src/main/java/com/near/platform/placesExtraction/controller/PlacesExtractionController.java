bb5cc85a6ece009f7aac2091822c10b9232cede1
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-09 18:01:47 +0530

    mailer functionality added

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -2,25 +2,33 @@ package com.near.platform.placesExtraction.controller;
 
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.service.PlacesExtractionService;
+
 import io.swagger.annotations.ApiOperation;
+import mailer.NearMailerService;
 import mcm.NearServiceResponseDto;
+import mcm.NearServiceResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
 import java.util.List;
+
 
 @CrossOrigin
 @RestController
 @RequestMapping("/v1/places")
 public class PlacesExtractionController {
 
-    Logger logger = LoggerFactory.getLogger(PlacesExtractionController.class);
-
     @Autowired
     PlacesExtractionService placesExtractionService;
+
+    Logger logger = LoggerFactory.getLogger(PlacesExtractionController.class);
+
 
     @ApiOperation(value = "Add metrics fields into Mongodb database")
     @PostMapping(value = "/metrics")
@@ -30,7 +38,7 @@ public class PlacesExtractionController {
 
     @ApiOperation(value = "Update metrics field data ")
     @PutMapping(value ="/metrics/")
-    public ResponseEntity<NearServiceResponseDto> updateMetricsData( @RequestParam(value = "id") Long id,@RequestBody LocationMetrics locationMetrics) throws Exception {
+    public ResponseEntity<NearServiceResponseDto> updateMetricsData( @RequestParam(required = true, value = "id") Long id,@RequestBody LocationMetrics locationMetrics) throws Exception {
         return placesExtractionService.updateMetricsDetail(id, locationMetrics);
     }
 
@@ -38,7 +46,7 @@ public class PlacesExtractionController {
 
     @ApiOperation(value = "Fetch metrics data for given id")
     @GetMapping(value = "/metrics/")
-    public LocationMetrics getMetricsData(@RequestParam(value = "id") Long id) throws Exception {
+    public LocationMetrics getMetricsData(@RequestParam(required = true, value = "id") Long id) throws Exception {
         return placesExtractionService.getLocationMetricsData(id);
     }
 
@@ -47,5 +55,15 @@ public class PlacesExtractionController {
     @GetMapping(value = "/metrics/all")
     public List<LocationMetrics> getAllMetricsData() throws Exception{
         return placesExtractionService.getAllLocationMetricsData();
+    }
+
+
+    //todo mailer testing
+    @ApiOperation(value = "Add metrics data and send mail to client")
+    @RequestMapping(value = "/metrics/add/", method = RequestMethod.POST)
+    public ResponseEntity<NearServiceResponseDto> uploadDataToDatabase(@RequestBody LocationMetrics locationMetrics, @RequestParam(value = "userId") String userId) throws Exception {
+
+        return placesExtractionService.addFileDataToDatabase(locationMetrics,userId);
+
     }
 }
