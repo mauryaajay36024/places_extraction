5f179b25d57659e8845706db191c588f3c2676c0
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-10 15:05:36 +0530

    service modified

--- src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionService.java
+++ src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionService.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 public interface PlacesExtractionService {
   ResponseEntity<NearServiceResponseDto> addMetricsDataToDatabase(LocationMetrics locationMetrics) throws Exception;
-  ResponseEntity<NearServiceResponseDto> updateMetricsDetail(Long id, LocationMetrics locationMetrics) throws Exception;
-  LocationMetrics getLocationMetricsData(Long id) throws Exception;
-  List<LocationMetrics>  getAllLocationMetricsData() throws Exception;
 
   ResponseEntity<NearServiceResponseDto> addPlacesDataToDatabase(LocationMetrics locationMetrics, String userId) throws Exception;
+
+  ResponseEntity<NearServiceResponseDto> updateMetricsDetail(Long id, LocationMetrics locationMetrics,String userId) throws Exception;
+
+  LocationMetrics getLocationMetricsData(Long id) throws Exception;
+
+  List<LocationMetrics>  getAllLocationMetricsData() throws Exception;
+
 }
