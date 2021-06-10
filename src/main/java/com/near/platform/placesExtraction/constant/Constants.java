22ed3b2adb3510916d0e34533a83856af5753441
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-10 15:01:03 +0530

    Constants class updated

--- src/main/java/com/near/platform/placesExtraction/constant/Constants.java
+++ src/main/java/com/near/platform/placesExtraction/constant/Constants.java
@@ -3,5 +3,4 @@ package com.near.platform.placesExtraction.constant;
 public interface Constants {
   String MAILER_FROM_ADDRESS = "no-reply@adnear.net";
   String SUCCESS_SUBJECT = "Request for data upload to database was successful";
-  String SUCCESS_BODY = "Dear user,<br><br>Your request for data upload is complete.<br>";
 }
