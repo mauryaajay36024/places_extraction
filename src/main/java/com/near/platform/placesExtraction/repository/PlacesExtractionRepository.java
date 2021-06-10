f382988c32b56b9275c2606613ee2284579012ff
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-10 15:04:57 +0530

    Repository modified

--- src/main/java/com/near/platform/placesExtraction/repository/PlacesExtractionRepository.java
+++ src/main/java/com/near/platform/placesExtraction/repository/PlacesExtractionRepository.java
@@ -3,9 +3,6 @@ package com.near.platform.placesExtraction.repository;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
-
-
 
 @Repository
 public interface PlacesExtractionRepository extends MongoRepository<LocationMetrics, Long> {
