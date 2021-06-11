b470d2f33816c1f2e60849dafcc922754655bd2c
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-11 17:59:04 +0530

    FileProcessRequest added

---
+++ src/main/java/com/near/platform/placesExtraction/dto/request/FileProcessRequest.java
@@ -0,0 +1,87 @@
+package com.near.platform.placesExtraction.dto.request;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FileProcessRequest {
+
+  private Map<String,Object> conf;
+  private Integer numExecutors;
+  private String file;
+  private String executorMemory;
+  private String driverMemory;
+  private Integer executorCores;
+//  private List<String>  pyFiles;
+
+
+  public FileProcessRequest(Map<String, Object> conf, Integer numExecutors, String file, String executorMemory, String driverMemory, Integer executorCores) {
+    this.conf = conf;
+    this.numExecutors = numExecutors;
+    this.file = file;
+    this.executorMemory = executorMemory;
+    this.driverMemory = driverMemory;
+    this.executorCores = executorCores;
+  }
+
+  public Map<String, Object> getConf() {
+    return conf;
+  }
+
+  public void setConf(Map<String, Object> conf) {
+    this.conf = conf;
+  }
+
+  public Integer getNumExecutors() {
+    return numExecutors;
+  }
+
+  public void setNumExecutors(Integer numExecutors) {
+    this.numExecutors = numExecutors;
+  }
+
+  public String getFile() {
+    return file;
+  }
+
+  public void setFile(String file) {
+    this.file = file;
+  }
+
+  public String getExecutorMemory() {
+    return executorMemory;
+  }
+
+  public void setExecutorMemory(String executorMemory) {
+    this.executorMemory = executorMemory;
+  }
+
+  public String getDriverMemory() {
+    return driverMemory;
+  }
+
+  public void setDriverMemory(String driverMemory) {
+    this.driverMemory = driverMemory;
+  }
+
+  public Integer getExecutorCores() {
+    return executorCores;
+  }
+
+  public void setExecutorCores(Integer executorCores) {
+    this.executorCores = executorCores;
+  }
+//todo remove commented code
+//  public List<String> getPyFiles() {
+//    return pyFiles;
+//  }
+//
+//  public void setPyFiles(List<String> pyFiles) {
+//    this.pyFiles = pyFiles;
+//  }
+}
