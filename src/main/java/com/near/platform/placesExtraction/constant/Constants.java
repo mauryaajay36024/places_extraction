9721d921d35d0697b5c211dbd8b48bf24d05ab0e
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-14 17:49:39 +0530

    constants modified

--- src/main/java/com/near/platform/placesExtraction/constant/Constants.java
+++ src/main/java/com/near/platform/placesExtraction/constant/Constants.java
@@ -3,4 +3,5 @@ package com.near.platform.placesExtraction.constant;
 public interface Constants {
   String MAILER_FROM_ADDRESS = "no-reply@adnear.net";
   String SUCCESS_SUBJECT = "Request for data upload to database was successful";
+  String REDIS_KEY= "livyJobQueue";
 }
