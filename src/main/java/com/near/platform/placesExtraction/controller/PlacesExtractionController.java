75a7288442b59dee62466abb4180004f664485fd
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-15 16:30:36 +0530

    placesExtractionController modified

--- src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
+++ src/main/java/com/near/platform/placesExtraction/controller/PlacesExtractionController.java
@@ -4,8 +4,6 @@ import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import com.near.platform.placesExtraction.service.PlacesExtractionService;
 import io.swagger.annotations.ApiOperation;
 import mcm.NearServiceResponseDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,47 +18,36 @@ public class PlacesExtractionController {
     @Autowired
     PlacesExtractionService placesExtractionService;
 
-    Logger logger = LoggerFactory.getLogger(PlacesExtractionController.class);
-
-
-    @ApiOperation(value = "Add metrics fields into Mongodb database")
-    @PostMapping(value = "/metrics")
-    public ResponseEntity<NearServiceResponseDto> addMetricsData(@RequestBody LocationMetrics locationMetrics) throws Exception {
-        return placesExtractionService.addMetricsDataToDatabase(locationMetrics);
-    }
-
     @ApiOperation(value = "Add metrics data and send mail to client")
-    @RequestMapping(value = "/metrics/add", method = RequestMethod.POST)
-    public ResponseEntity<NearServiceResponseDto> uploadDataToDatabase(@RequestBody LocationMetrics locationMetrics, @RequestParam(value = "userId") String userId) throws Exception {
+    @RequestMapping(value = "/metrics/insert", method = RequestMethod.POST)
+    public ResponseEntity<NearServiceResponseDto> uploadDataToDatabase(@RequestBody LocationMetrics locationMetrics, @RequestParam(value = "emailId") String userId) throws Exception {
         return placesExtractionService.addPlacesDataToDatabase(locationMetrics,userId);
 
     }
 
     @ApiOperation(value = "Update metrics field data and send mail to user ")
-    @PutMapping(value ="/metrics")
-    public ResponseEntity<NearServiceResponseDto> updateMetricsData( @RequestParam(required = true, value = "id") Long id,@RequestBody LocationMetrics locationMetrics,@RequestParam(value = "userId") String userId) throws Exception {
+    @PutMapping(value ="/metrics/update")
+    public ResponseEntity<NearServiceResponseDto> updateMetricsData( @RequestParam(value = "id") Long id,@RequestBody LocationMetrics locationMetrics,@RequestParam(value = "emailId") String userId) throws Exception {
         return placesExtractionService.updateMetricsDetail(id, locationMetrics,userId);
     }
 
-    //Fetch location metrics data
     @ApiOperation(value = "Fetch metrics data for given id")
-    @GetMapping(value = "/metrics")
-    public LocationMetrics getMetricsData(@RequestParam(required = true, value = "id") Long id) throws Exception {
+    @GetMapping(value = "/metrics/fetchSingleData")
+    public LocationMetrics getMetricsData(@RequestParam(value = "id") Long id) throws Exception {
         return placesExtractionService.getLocationMetricsData(id);
     }
 
-    //Fetch all location metrics data
     @ApiOperation(value = "Fetch all metrics data")
-    @GetMapping(value = "/metrics/all")
+    @GetMapping(value = "/metrics/fetchAllData")
     public List<LocationMetrics> getAllMetricsData() throws Exception{
         return placesExtractionService.getAllLocationMetricsData();
     }
 
 
     @ApiOperation(value = "livy call to start a spark job")
-    @GetMapping(value = "/livy")
-    public ResponseEntity<NearServiceResponseDto> livy() throws Exception{
-        return placesExtractionService.livyStartSparkJob();
+    @GetMapping(value = "/livy/runScript")
+    public ResponseEntity<NearServiceResponseDto> livy(@RequestParam(value = "POIListId") String poiListId) throws Exception{
+        return placesExtractionService.livyStartSparkJob(poiListId);
     }
 
     @ApiOperation(value = "Redis call to pop request and start livy job via script")
