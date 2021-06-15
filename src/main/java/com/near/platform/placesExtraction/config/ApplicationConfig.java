8e5e959a95d59bf083073fd2cc716864b2f5a3eb
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-15 16:29:01 +0530

    livy config added

--- src/main/java/com/near/platform/placesExtraction/config/ApplicationConfig.java
+++ src/main/java/com/near/platform/placesExtraction/config/ApplicationConfig.java
@@ -46,23 +46,59 @@ public class ApplicationConfig {
   }
 
   public static class Livy {
-    private String host;
-    private String createBatchEndpoint;
+    private String url;
+    private String driverMemory;
+    private String executorMemory;
+    private Integer numExecutors;
+    private String file;
+    private Integer executorCores;
 
-    public String getHost() {
-      return host;
+    public String getUrl() {
+      return url;
     }
 
-    public void setHost(String host) {
-      this.host = host;
+    public void setUrl(String url) {
+      this.url = url;
     }
 
-    public String getCreateBatchEndpoint() {
-      return createBatchEndpoint;
+    public String getDriverMemory() {
+      return driverMemory;
     }
 
-    public void setCreateBatchEndpoint(String createBatchEndpoint) {
-      this.createBatchEndpoint = createBatchEndpoint;
+    public void setDriverMemory(String driverMemory) {
+      this.driverMemory = driverMemory;
+    }
+
+    public String getExecutorMemory() {
+      return executorMemory;
+    }
+
+    public void setExecutorMemory(String executorMemory) {
+      this.executorMemory = executorMemory;
+    }
+
+    public Integer getNumExecutors() {
+      return numExecutors;
+    }
+
+    public void setNumExecutors(Integer numExecutors) {
+      this.numExecutors = numExecutors;
+    }
+
+    public String getFile() {
+      return file;
+    }
+
+    public void setFile(String file) {
+      this.file = file;
+    }
+
+    public Integer getExecutorCores() {
+      return executorCores;
+    }
+
+    public void setExecutorCores(Integer executorCores) {
+      this.executorCores = executorCores;
     }
   }
 
