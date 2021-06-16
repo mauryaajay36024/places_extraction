fc1c524dd1950d303fe7deb255dd0a7b8263a4a7
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-16 08:57:36 +0530

    constants added

--- src/main/java/com/near/platform/placesExtraction/constant/Constants.java
+++ src/main/java/com/near/platform/placesExtraction/constant/Constants.java
@@ -5,4 +5,6 @@ public interface Constants {
   String SUCCESS_SUBJECT = "Request for data upload/update to database was successful";
   String FAILURE_SUBJECT = "Request for data upload/update failed";
   String REDIS_KEY= "livyJobQueue";
+  String LIVY_SUCCESS_SUBJECT = "Livy request execution was successful";
+  String LIVY_FAILURE_SUBJECT = "Livy request execution failed";
 }
