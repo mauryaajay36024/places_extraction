4a962033772d10003ed0b105192f89e5bb6d98b3
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-16 11:30:49 +0530

    constants added

--- src/main/java/com/near/platform/placesExtraction/constant/Constants.java
+++ src/main/java/com/near/platform/placesExtraction/constant/Constants.java
@@ -4,6 +4,8 @@ public interface Constants {
   String MAILER_FROM_ADDRESS = "no-reply@adnear.net";
   String SUCCESS_SUBJECT = "Request for data upload/update to database was successful";
   String FAILURE_SUBJECT = "Request for data upload/update failed";
+  String UPDATE_FAILURE_BODY = "Dear user,<br><br>Your request for data update is failed.<br>";
+  String UPDATE_SUCCESS_BODY = "Dear user,<br><br>Your request for data update is complete.<br>"+ "<br>";
   String REDIS_KEY= "livyJobQueue";
   String LIVY_SUCCESS_SUBJECT = "Livy request execution was successful";
   String LIVY_FAILURE_SUBJECT = "Livy request execution failed";
