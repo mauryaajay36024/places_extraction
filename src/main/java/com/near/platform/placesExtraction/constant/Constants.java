a21fc2beff7bf0fdc3537bf9e3250da41640f088
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-17 15:01:20 +0530

    constants updated

--- src/main/java/com/near/platform/placesExtraction/constant/Constants.java
+++ src/main/java/com/near/platform/placesExtraction/constant/Constants.java
@@ -2,14 +2,15 @@ package com.near.platform.placesExtraction.constant;
 
 public interface Constants {
   String MAILER_FROM_ADDRESS = "no-reply@adnear.net";
-  String SUCCESS_SUBJECT = "Request for data upload/update to database was successful";
-  String FAILURE_SUBJECT = "Request for data upload/update failed";
-  String UPDATE_FAILURE_BODY = "Dear user,<br><br>Your request for data update is failed.<br>";
-  String UPDATE_SUCCESS_BODY = "Dear user,<br><br>Your request for data update is complete.<br>"+ "<br>";
+  String SUCCESS_SUBJECT = "Request for data insert/update to database was successful";
+  String FAILURE_SUBJECT = "Request for data insert/update failed";
   String REDIS_KEY= "livyJobQueue";
   String LIVY_SUCCESS_SUBJECT = "Livy request execution was successful";
   String LIVY_FAILURE_SUBJECT = "Livy request execution failed";
   String LIVY_SUCCESS_BODY = "Dear user,<br><br>Livy request from queue executed successfully.<br>";
   String LIVY_FAILURE_BODY = "Dear user,<br><br>Previous livy request failed. Now executing next request from queue<br>";
   String PLACES_TEAM_EMAIL = "ajay@near.co";
+  String MAX_APP_ATTEMPTS ="spark.yarn.maxAppAttempts" ;
+  String MIN_EXECUTORS = "spark.dynamicAllocation.minExecutors";
+  String MAX_EXECUTORS = "spark.dynamicAllocation.maxExecutors";
 }
