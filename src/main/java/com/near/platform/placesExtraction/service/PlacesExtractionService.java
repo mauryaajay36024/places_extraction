386680059d6ae9aa450b3e165743146fcc47ef86
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-06-17 15:07:21 +0530

    service updated

--- src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionService.java
+++ src/main/java/com/near/platform/placesExtraction/service/PlacesExtractionService.java
@@ -3,20 +3,17 @@ package com.near.platform.placesExtraction.service;
 import com.near.platform.placesExtraction.model.mongo.LocationMetrics;
 import mcm.NearServiceResponseDto;
 import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
 public interface PlacesExtractionService {
 
   ResponseEntity<NearServiceResponseDto> addPlacesDataToDatabase(LocationMetrics locationMetrics, String userId) throws Exception;
 
-  ResponseEntity<NearServiceResponseDto> updateMetricsDetail(Long id, LocationMetrics locationMetrics,String userId) throws Exception;
+  List<LocationMetrics> getLocationMetricsDataForId(LocationMetrics locationMetrics) throws Exception;
 
-  LocationMetrics getLocationMetricsData(Long id) throws Exception;
-
-  List<LocationMetrics>  getAllLocationMetricsData() throws Exception;
+  List<LocationMetrics>  getAllLocationMetricsData(boolean latestOnly) throws Exception;
 
   ResponseEntity<NearServiceResponseDto> livyExecutePysparkJob(String poiListId) throws Exception;
 
-  ResponseEntity<NearServiceResponseDto> executeLivyJobFromQueue(boolean jobStatus) throws Exception;
+  ResponseEntity<NearServiceResponseDto> executeLivyJobFromRedis(boolean jobStatus) throws Exception;
 }
